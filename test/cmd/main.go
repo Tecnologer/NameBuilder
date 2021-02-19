014ef396afafb3c6b6edef6e0151812508d8022c
tecnologer <rdominguez@tecnologer.net>
2021-02-19 15:09:25 -0600

    add flags to CLI

--- test/cmd/main.go
+++ test/cmd/main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 
 	"github.com/tecnologer/NameBuilder/src"
 )
 
-func main() {
+var (
+	count      = flag.Int("count", 1, "how many names should be generated")
+	nameLen    = flag.Int("len", 4, "length of the name(s)")
+	minNameLen = flag.Int("len-min", 4, "the minimum of lenght of the name(s)")
+)
 
-	for i := 0; i < 10; i++ {
-		name, err := src.GetRandomName(rand.Intn(6) + 4)
+func main() {
+	flag.Parse()
+	var l int
+	for i := 0; i < *count; i++ {
+		l = *nameLen
+		if *minNameLen != *nameLen {
+			l = rand.Intn(*nameLen-*minNameLen) + *minNameLen
+		}
+
+		name, err := src.GetRandomName(l)
 		if err != nil {
+			fmt.Println(err)
 			continue
 		}
 
