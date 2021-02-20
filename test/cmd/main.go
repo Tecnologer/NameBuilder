c8c3f1f9d51a6a068f3571574b666c7e0f7097b6
Rey David <rdominguez@tecnologer.net>
2021-02-20 00:23:50 -0600

    main: call random with seed flag, add profiling

--- test/cmd/main.go
+++ test/cmd/main.go
@@ -5,16 +5,20 @@ import (
 	"fmt"
 	"math/rand"
 
-	"github.com/tecnologer/NameBuilder/src"
+	"github.com/pkg/profile"
+	randName "github.com/tecnologer/NameBuilder/src"
 )
 
 var (
 	count      = flag.Int("count", 1, "how many names should be generated")
 	nameLen    = flag.Int("len", 4, "length of the name(s)")
 	minNameLen = flag.Int("len-min", 4, "the minimum of lenght of the name(s)")
+	seed       = flag.String("seed", "", "the minimum of lenght of the name(s)")
 )
 
 func main() {
+	defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
+
 	flag.Parse()
 	var l int
 	for i := 0; i < *count; i++ {
@@ -23,7 +27,7 @@ func main() {
 			l = rand.Intn(*nameLen-*minNameLen) + *minNameLen
 		}
 
-		name, err := src.GetRandomName(l)
+		name, err := randName.GetRandomWithSeed(*seed, l)
 		if err != nil {
 			fmt.Println(err)
 			continue
